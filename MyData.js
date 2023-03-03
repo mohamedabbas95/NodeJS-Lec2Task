075803bead31cbd69c8298c77312256a1f12599d
mohamedabbas95 <aboabbas944@gmail.com>
2023-03-03 15:21:40 +0200

    Second Commit Array Task

--- MyData.js
+++ MyData.js
@@ -73,11 +73,50 @@
             console.log(obj.fname,obj.lname , obj.age,obj.city)
         })
     }
-     
+////////////////////////////////////////////////////////////////////
+ const calculateDegree = (id,degrees) =>{
+    const allData = loadData();
+    let total = 0;
+    let avg = 0;
+    let flag =true;
+    // get the Desired Student TO calculate his Degree  
+    const student = allData.find((obj) => {
+        return obj.id == id
+    })
+    //check if Id exsist
+    if (student){
+            degrees.forEach((n)=>{
+                if (n >= 0 && n <= 100) {
+                  total += parseInt(n);
+                }else{
+                    flag = false;
+                }
+                
+            })
+            if(flag){
+                avg = total / degrees.length;
+                avg = avg.toFixed(2);
+                student.total = total;
+                student.avg = avg;
+                console.log(student.fname, student.lname, student.age, student.city, "Total Degree Is :", total, "And Average Is :", avg);
+                saveAllData(allData);
+                
+            }else{
+                console.log("You should Enter Correct degree In Range from 0 To 100");
+            }
+            
+    }else{
+        console.log("You Have Entered An Id that Dose Not Exsist");
+    }
+ }
+
+
+//////////////////////////////////////////////////////////////////////
 
 module.exports = {
     addPerson,
     deleteData, 
     readData,
-    listData
+    listData,
+    calculateDegree
 }
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const MyData = require ("./MyData")
 const yargs = require("yargs")
+const validator = require("validator")
 yargs.command({
     command : "add",
     describe: "to add an item",
@@ -42,6 +43,30 @@ yargs.command({
         MyData.readData (x.id)
     }
  })
+
+  yargs.command({
+      command: "degree",
+      describe: "to calculate total and average",
+      builder: {
+          id: {
+              describe: "this is id description in degree command",
+              demandOption: true,
+              type: "string"
+          },
+          degArr: {
+              describe: "this is id description in degree command",
+              demandOption: true,
+              type: "string"
+          }
+
+      },
+      handler: (x) => {
+         const ArrayConvert = x.degArr.split(",");
+         MyData.calculateDegree(x.id, ArrayConvert)
+          //console.log(validator.isNumeric(ArrayConvert[0]),ArrayConvert[0])
+          //console.log(parseInt(ArrayConvert[0]) + parseInt(ArrayConvert[1]))
+      }
+  })
 
 
  yargs.command ({
